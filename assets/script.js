59ff12014d863b060cca4849320416db191c96d2
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-01-10 20:06:39 -0500

    added the youtube api

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // youtube API key
 const key = "AIzaSyA4D1jpi2mpVlAlUO9TWKG2mxPCDFda1l4"
 
-var youTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs&type=video&key=AIzaSyA4D1jpi2mpVlAlUO9TWKG2mxPCDFda1l4"
 
 // Examples so I don't forget 
 // GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=[YOUR_API_KEY] HTTP/1.1
@@ -11,21 +10,22 @@ var youTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs&
 
 // https://www.youtube.com/watch?v=-WowH0liGfE
 $("button").on("click", function( ){
-  console.log("click ",this.value)
+  var input = $("#videoId").val()
+  console.log("click ", input)
+  
+  var youTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ input +"&type=video&key="+ key
+  fetch(youTube)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.items[0].id.videoId);
+    // displayYouTube();
+  });
+  
+  
 })
-var input = $("#videoId").value 
-
-fetch(youTube)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        console.log(data.items[0].id.videoId);
-        // displayYouTube();
-      });
-
-    
       
 
 // function displayYouTube(){}
