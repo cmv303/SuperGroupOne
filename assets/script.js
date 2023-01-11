4d805676e5cacf6690589167ab9b692718761c40
Wanamaker919 <116662904+Wanamaker919@users.noreply.github.com>
2023-01-10 21:08:07 -0500

    YT search done

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,17 @@
 const key = "AIzaSyA4D1jpi2mpVlAlUO9TWKG2mxPCDFda1l4"
 
 
+function displayYouTube(ytList){
+  $("#resultsList").empty("")
+  var list = $("<ul>")
+  for (let i = 0; i < ytList.length; i++) {
+    var listItem = $("<li>")
+    listItem.text(ytList[i].snippet.title)
+    list.append(listItem) 
+  }
+$("#resultsList").append(list)
+}
+
 // Examples so I don't forget 
 // GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=Ks-_Mh1QhMc&type=video&key=[YOUR_API_KEY] HTTP/1.1
 
@@ -9,23 +20,26 @@ const key = "AIzaSyA4D1jpi2mpVlAlUO9TWKG2mxPCDFda1l4"
 // Accept: application/json
 
 // https://www.youtube.com/watch?v=-WowH0liGfE
+function youTubeAPI(input){
+
+var youTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ input +"&type=video&key="+ key
+fetch(youTube)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+  console.log(data.items[0].id.videoId);
+  displayYouTube(data.items);
+});
+
+}
+
+
 $("button").on("click", function( ){
   var input = $("#videoId").val()
   console.log("click ", input)
-  
-  var youTube = "https://www.googleapis.com/youtube/v3/search?part=snippet&q="+ input +"&type=video&key="+ key
-  fetch(youTube)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.items[0].id.videoId);
-    // displayYouTube();
-  });
-  
-  
-})
-      
+  youTubeAPI(input)
+}) 
 
 // function displayYouTube(){}
